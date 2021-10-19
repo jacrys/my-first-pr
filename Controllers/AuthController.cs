83d3586140ee4addd2cbeb3f423de9b2cac5a09d
Jacrys <kclewis0614@gmail.com>
2021-10-19 13:05:09 -0500

    troubleshoot debug

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,11 +3,18 @@
     [Route("[controller]/[action]")]
     public class AuthController : Controller
     {
+        private readonly ILogger _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult Login(string returnUrl = "/")
         {
 #if DEBUG
-            Console.WriteLine(returnUrl);
+            _logger.LogError(returnUrl);
 #endif
             return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
         }
